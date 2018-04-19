28ec28e66e3a4addc66b589228a1a20d761a1fc1
Emily Waite <ewaite@cca.edu>
2018-04-19 00:36:32 -0700

    hello_almostthere?

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,11 @@
-/*
-function setup() {
-var canvas = createCanvas(1962, 1500);
-canvas.parent('containertwo');
+
+/* function setup() {
+var myCanvas = createCanvas(1962, 1700);
+    myCanvas.parent("bigasscircles");
+
 }
 
 function draw() {
-noFill();
-background(0, 0, 0);
-stroke(255, 102, 0);
-stroke(153, 204, 204);
-bezier(985, 200, 10, 10, 10, 90, 1000, 800);
+ellipse(654, 654, 2000, 2000);
 }
 
-*/
