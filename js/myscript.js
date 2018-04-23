1e259cbc592ed954972f9ae753744f18521dc749
Emily Waite <ewaite@cca.edu>
2018-04-22 17:41:46 -0700

    more coding

--- js/myscript.js
+++ js/myscript.js
@@ -1,49 +1,134 @@
 /*$(document).ready(function(){
 
+	/*gastro>xray
+	$(".storyseven").hide();
+	$(".line7").hide();
+	$(".boxseven").hide();
+	$(".xray").hide();
+	$(".storysix").show();
 
-	$(".storyfour").hide();
-	$(".line5").hide();
-	$(".storythree").show();
-
-
-	$('.storythree').click(function(){
-	$(".storyfour").fadeToggle(2000);
-	$(".line5").delay(1500).fadeToggle(2000);
+	$('.storysix').click(function(){
+		$(".boxseven").addClass("zdex") .fadeToggle(1400);
 	});
 
-    
-	$(".storythree").hide();
-	$(".storytwo").show();
-
-	$('.storytwo').click(function(){
-	$(".storythree").fadeToggle(2000);
+	$('.boxseven').click(function(){
+		$(".boxseven").removeClass("zdex");
+		$(".storyseven").fadeToggle(1400);
+		$(".xray").delay(600).fadeToggle(1400);
+		$(".line7").delay(1400).fadeToggle(1400);
 	
 	});
 
+	/*nutritionist>gastro>
+	$(".storysix").hide();
+	$(".line6").hide();
+	$(".boxsix").hide();
+	$(".storyfive").show();
 
+	$('.storyfive').click(function(){
+		$(".boxsix").addClass("zdex") .fadeToggle(1400);
+	});
+
+	$('.boxsix').click(function(){
+		$(".boxsix").removeClass("zdex");
+		$(".storysix").fadeToggle(1400);
+		$(".line6").delay(1400).fadeToggle(1400);
+	
+	});
+
+	/*gastro>nutritionist
+	$(".storyfive").hide();
+	$(".line5").hide();
+	$(".boxfive").hide();
+	$(".nutritionist").hide();
+	$(".storyfour").show();
+
+	$('.storyfour').click(function(){
+		$(".boxfive").addClass("zdex") .fadeToggle(1400);
+	});
+
+	$('.boxfive').click(function(){
+		$(".boxfive").removeClass("zdex");
+		$(".storyfive").fadeToggle(1400);
+		$(".nutritionist").delay(600).fadeToggle(1400);
+		$(".line5").delay(1400).fadeToggle(1400);
+	
+	});
+
+	/*second MD*> gastro
+
+	$(".storyfour").hide();
+	$(".line4").hide();
+	$(".boxfour").hide();
+	$(".gastro").hide();
+	$(".storythree").show();
+
+	$('.storythree').click(function(){
+		$(".boxfour").addClass("zdex") .fadeToggle(1400);
+	});
+
+	$('.boxfour').click(function(){
+		$(".boxfour").removeClass("zdex");
+		$(".storyfour").fadeToggle(1400);
+		$(".gastro").delay(600).fadeToggle(1400);
+		$(".line4").delay(1400).fadeToggle(1400);
+	
+	});
+	
+
+    /*Presciptions and blood testing > second MD
+	$(".storythree").hide();
+	$(".line3").hide();
+	$(".boxtwo").hide();
+	$(".boxthree").hide();
+	$(".storytwo").show();
+
+
+	$('.storytwo').click(function(){
+		$(".boxtwo").addClass("zdex") .fadeToggle(1400);
+	});
+
+	$('.boxtwo').click(function(){
+		$(".boxtwo").removeClass("zdex");
+	});
+
+	$('.storytwopt2').click(function(){
+		$(".boxthree").addClass("zdex") .fadeToggle(1400);
+	});	
+	
+	$('.boxthree').click(function(){
+		$(".boxthree").removeClass("zdex");
+		$(".storythree").fadeToggle(1400);
+		$(".line3").delay(600).fadeToggle(1400);
+	
+	});
+	
+	/*MD > Presciptions and blood testing
 	$(".storytwo").hide();
-	$(".texttwo").hide();
+	$(".storytwopt2").hide();
+	$(".prescriptions").hide();
+	$(".bloodtesting").hide();
 	$(".line").hide();
 	$(".line2").hide();
-	$(".stuf").hide();
 	$(".lightboxone").hide();
 	$(".storyone").show();
 
-
 	$('.storyone').click(function(){
-	$(".lightboxone").addClass("zdex") .fadeToggle(2000);;
-	$(".stuf").delay(1000).fadeToggle(2000);
+		$(".lightboxone").addClass("zdex") .fadeToggle(1400);
+	
 	});
 	
 	$('.lightboxone').click(function(){
-	$(".lightboxone").removeClass("zdex");
-	$(".storytwo").fadeToggle(2000);
-	$(".texttwo").delay(1000).fadeToggle(2000);
-	$(".line").delay(2000).fadeToggle(2000);
-	$(".line2").delay(2000).fadeToggle(2000);
+		$(".lightboxone").removeClass("zdex");
+		$(".storytwo").fadeToggle(1400);
+		$(".storytwopt2").fadeToggle(1400);
+		$(".prescriptions").delay(600).fadeToggle(1400);
+		$(".bloodtesting").delay(600).fadeToggle(1400);
+		$(".line").delay(1400).fadeToggle(1400);
+		$(".line2").delay(1400).fadeToggle(1400);
 	});
  
-});
+}); */
 
 
 
